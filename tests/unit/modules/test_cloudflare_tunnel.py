400e0a5bed4554e091840bc5830112833086ab78
Martin Fedec <7654942+mfedec@users.noreply.github.com>
2023-01-31 21:07:39 -0800

    Few basic tests
    Trying to figure out how all this works.

--- tests/unit/modules/test_cloudflare_tunnel.py
+++ tests/unit/modules/test_cloudflare_tunnel.py
@@ -1,8 +1,8 @@
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
+
 import pytest
-import salt.modules.linux_service as service
 import saltext.cloudflare_tunnel.modules.cloudflare_tunnel_mod as cloudflare_tunnel_module
 
 
@@ -18,15 +18,64 @@ def configure_loader_modules():
     }
 
 
+@pytest.fixture
+def fake_cloudflare_auth():
+    with patch("Cloudflare.Cloudflare"):
+        return ["Cloudflare", "REDACTED"]
+
+
+def test_install_connector():
+    with patch.object(cloudflare_tunnel_module, "_get_tunnel_token", MagicMock(return_value="12345")):
+        with patch.dict(cloudflare_tunnel_module.__salt__, {"cmd.run": MagicMock(return_value="installed successfully")}):
+            assert cloudflare_tunnel_module.install_connector('12345') is True
+
+
+def test_install_connector_failed():
+    with patch.object(cloudflare_tunnel_module, "_get_tunnel_token", MagicMock(return_value="12345")):
+        with patch.dict(cloudflare_tunnel_module.__salt__, {"cmd.run": MagicMock(return_value="provided token is invalid")}):
+            assert cloudflare_tunnel_module.install_connector('12345') is False
+
+
 def test_is_service_available():
     with patch.dict(
         cloudflare_tunnel_module.__salt__, {"service.available": MagicMock(return_value=True)}
     ):
-        assert cloudflare_tunnel_module.is_connector_installed() == True
+        assert cloudflare_tunnel_module.is_connector_installed() is True
 
 
 def test_is_service_not_available():
     with patch.dict(
         cloudflare_tunnel_module.__salt__, {"service.available": MagicMock(return_value=False)}
     ):
-        assert cloudflare_tunnel_module.is_connector_installed() == False
+        assert cloudflare_tunnel_module.is_connector_installed() is False
+
+
+def test_remove_connector_if_not_available():
+    with patch.dict(
+        cloudflare_tunnel_module.__salt__, {"service.available": MagicMock(return_value=False)}
+    ):
+        assert cloudflare_tunnel_module.remove_connector() is True
+
+
+def test_remove_connector_if_available():
+    with patch.dict(
+        cloudflare_tunnel_module.__salt__,
+        {
+            "service.available": MagicMock(return_value=True),
+            "cmd.run": MagicMock(return_value="uninstalled successfully"),
+        },
+    ):
+        assert cloudflare_tunnel_module.remove_connector() is True
+
+
+def test_remove_connector_failed():
+    with patch.dict(
+        cloudflare_tunnel_module.__salt__,
+        {
+            "service.available": MagicMock(return_value=True),
+            "cmd.run": MagicMock(
+                return_value="Failed to disable unit: Unit file cloudflared.service does not exist."
+            ),
+        },
+    ):
+        assert cloudflare_tunnel_module.remove_connector() is False
